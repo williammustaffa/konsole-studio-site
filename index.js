b463a1bf62d5f03822db1644687f3171df18f354
williammustaffa <william.lima.cat@gmail.com>
2020-03-02 13:06:34 -0300

    test

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use('/', express.static(__dirname + '/public'));
+app.use('/', express.static(pth.join(__dirname, 'public')));
 
-app.listen(port, function(){
+app.listen(port, function() {
   console.log(`listening on *:${port}`);
 });
